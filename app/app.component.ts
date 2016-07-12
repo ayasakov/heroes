7c83698662b16de3dcf3d785f54dcc5c4791afc3
Artem Yasakov <ayasakov@mirantis.com>
2016-07-12 17:29:07 +0300

    Initial

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+// app/app.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: "./sub.html"
+})
+export class AppComponent { }
---
+++ app/boot.ts
@@ -0,0 +1,5 @@
+// app/boot.ts
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component'
+
+bootstrap(AppComponent, []);
